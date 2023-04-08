7edbed1e5d93c8f13e19690694e3e6ab70c74593
saravananhashref <saravanan@hashref.in>
2023-04-08 21:45:52 +0530

    new

---
+++ mini/src/Components/menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function menu() {
+  return (
+    <></>
+  )
+}
+
+export default menu;
---
+++ mini/src/Components/sideData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const sideData = [{
+        title: 'Pomodoro',
+        path: '/',
+        cName: 'nav-text'
+    },
+    {
+        title: '52-17',
+        path: '/52-17',
+        cName: 'nav-text'
+
+    },
+    {
+        title: 'Rhythm',
+        path: '/rhythm',
+        cName: 'nav-text'
+
+    },
+    {
+        title: 'Wish',
+        path: '/wish',
+        cName: 'nav-text'
+    }
+]
