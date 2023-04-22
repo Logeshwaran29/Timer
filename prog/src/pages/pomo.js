ae6028b6d6057682b17f698c208b9bddd88251b8
saravananhashref <saravanan@hashref.in>
2023-04-22 11:56:54 +0530

    new

---
+++ prog/src/pages/pomo.js
@@ -0,0 +1,92 @@
+import './pomo.css';
+import React, { useState, useEffect } from 'react';
+
+const PomodoroTimer = () => {
+  const [timeLeft, setTimeLeft] = useState(0.2 * 60); // 25 minutes in seconds
+  const [breakTimeLeft, setBreakTimeLeft] = useState(0.1 * 60); // 5 minutes in seconds
+  const [isRunning, setIsRunning] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
+  const [isPlay,setIsPlay] = useState(false);
+  const a=document.querySelector('#audio');
+ 
+
+  useEffect(() => {
+    let interval = null;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTimeLeft(prevTimeLeft => prevTimeLeft - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      if(!isPlay){
+        a.play();
+        setIsPlay(true);
+      }
+      setIsRunning(false);
+      if(isBreak){
+        setIsBreak(false);
+        setTimeLeft(25*60);
+      }else{
+        setIsBreak(true);
+        setTimeLeft(breakTimeLeft);
+      }
+    }
+  }, [timeLeft, breakTimeLeft, isBreak]);
+
+  const startTimer = () => {
+    if(isPlay){
+      a.pause();
+      a.currentTime=0;
+    }
+    setIsBreak(false);
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    if(isPlay){
+      a.pause();
+      a.currentTime=0;
+    }
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    a.pause();
+    a.currentTime=0;
+    setTimeLeft(25 * 60);
+    setBreakTimeLeft(5 * 60);
+    setIsRunning(false);
+    setIsBreak(false);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    document.title= `${minutes}:${seconds}`+" Timer";
+    return `${minutes}:${seconds}`;
+  };
+
+
+  return (
+    <div id="main">
+    <div id="div">
+      <audio id="audio">
+        <source src="alarm.mp3" type='audio/mpeg'/>
+      </audio>
+      <h1>{isBreak ? 'Break Time!' : 'Pomodoro Time!'}</h1>
+      <h2>{formatTime(timeLeft)}</h2>
+      <button class="button" onClick={startTimer}><i class="fa-solid fa-play"></i></button>
+      <button class="button" onClick={stopTimer}><i class="fa-solid fa-pause"></i></button>
+      <button id="reset" onClick={resetTimer}><i class="fa-solid fa-rotate"></i></button>
+    </div>
+    </div>
+  );
+};
+
+export default PomodoroTimer;
