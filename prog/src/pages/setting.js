18ca7ca1f2886e1f687efeda10171d2e51996d2a
saravananhashref <saravanan@hashref.in>
2023-04-27 19:17:41 +0530

    new

--- prog/src/pages/setting.js
+++ prog/src/pages/setting.js
@@ -3,7 +3,18 @@ import './pomo.css';
 
 const setting=()=>{
     return (
-      <div id='div'><h1>setting</h1></div>
+      <div id='div'>
+        <label htmlFor="">Choose Backgroud:</label>
+        <select name="" id="">
+          <option value="back1.jpg"></option>
+          <option value=""><img src="back2.jpg" alt="back1" /></option>
+          <option value=""><img src="back3.jpg" alt="back1" /></option>
+          <option value=""><img src="back4.jpg" alt="back1" /></option>
+          <option value=""><img src="back5.jpg" alt="back1" /></option>
+          <option value=""><img src="back6.jpg" alt="back1" /></option>
+          <option value=""><img src="back7.jpg" alt="back1" /></option>
+        </select>
+        </div>
     );
 };
 
