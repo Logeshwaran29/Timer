dc8d6eba79cbbffcc0b245df764a8c3179afc535
saravananhashref <saravanan@hashref.in>
2023-05-11 21:49:04 +0530

    new

--- prog/src/pages/setting.js
+++ prog/src/pages/setting.js
@@ -2,17 +2,23 @@ import React from 'react';
 import './pomo.css';
 
 const setting=()=>{
+    const change=()=>{
+      const a=document.querySelector('#back').value;
+      
+      document.body.style.backgroundImage='url(`back${a}.jpg`)';
+    }
+  
     return (
-      <div id='div'>
-        <label htmlFor="">Choose Backgroud:</label>
-        <select name="" id="">
-          <option value="back1.jpg"></option>
-          <option value=""><img src="back2.jpg" alt="back1" /></option>
-          <option value=""><img src="back3.jpg" alt="back1" /></option>
-          <option value=""><img src="back4.jpg" alt="back1" /></option>
-          <option value=""><img src="back5.jpg" alt="back1" /></option>
-          <option value=""><img src="back6.jpg" alt="back1" /></option>
-          <option value=""><img src="back7.jpg" alt="back1" /></option>
+      <div id='div1'>
+        <label htmlFor="back">Choose Backgroud:</label>
+        <select name="" id="back">
+          <option value="1">Back1</option>
+          <option value="2">Back2</option>
+          <option value="3">Back3</option>
+          <option value="4">Back4</option>
+          <option value="5">Back5</option>
+          <option value="6">Back6</option>
+          <option value="7">Back7</option>
         </select>
         </div>
     );
